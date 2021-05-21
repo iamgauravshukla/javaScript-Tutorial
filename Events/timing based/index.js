25f55bc02a1bfb12b217df53e2518d0f5f97832b
iamgauravshukla <gauravshukla0008@gmail.com>
2021-05-21 09:21:46 +0530

    time based event  program added

---
+++ Events/timing based/index.js
@@ -0,0 +1,8 @@
+// setTimeout(function, millisecond)
+setTimeout(()=>{
+    alert("Hello User!");
+},1000)
+// setInterval(function, millisecond)
+setTimeout(()=>{
+    console.log("Hey");
+},5000);
